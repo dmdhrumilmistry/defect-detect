4bc8977d0c001d6b2d41429a8fcdd2d72a72e30a
dhyeythumar <dhyeythumar@gmail.com>
2025-01-01 18:22:57 +0530

    fea: helper services like rest service with cache proxy & cookie service

---
+++ frontend/src/services/cookie.ts
@@ -0,0 +1,19 @@
+import type { Maybe } from '../types';
+
+export default class CookieService {
+    static get(name: string): Maybe<string> {
+        const match = new RegExp('(^|;)\\s*' + encodeURIComponent(name) + '=([^;]*)').exec(document.cookie);
+        return match ? decodeURIComponent(match[2]) : null;
+    }
+
+    static set(name: string, value: string, maxAgeInSec?: number): void {
+        const defaultCookieSettings = 'secure=true; samesite=Strict; path=/';
+        let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; ${defaultCookieSettings}`;
+        if (maxAgeInSec) cookieString += `; max-age=${maxAgeInSec}`;
+        document.cookie = cookieString;
+    }
+
+    static delete(name: string): void {
+        this.set(name, '', -1);
+    }
+}
