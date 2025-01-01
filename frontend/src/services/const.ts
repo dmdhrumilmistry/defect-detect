1d4cc33b6321279f63fed30be4017d45cb3632c2
dhyeythumar <dhyeythumar@gmail.com>
2025-01-01 18:24:28 +0530

    misc: added const & updates types

---
+++ frontend/src/services/const.ts
@@ -0,0 +1,9 @@
+export const API_BASE_URL = 'https://dummyjson.com';
+
+export const CACHE_KEYS = {
+    userInfo: 'USER_INFO',
+};
+
+export const COOKIE_KEYS = {
+    userId: 'app.userId',
+};
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,3 +1,26 @@
-export type Maybe<T> = T | null;
+export type Maybe<T> = T | null | undefined;
 
 export type StringKVs = Record<string, string>;
+
+export type HttpMethod = 'PUT' | 'POST';
+
+export type FetchArgs = Readonly<{
+    url: string;
+    headers?: HeadersInit;
+    signal?: AbortSignal;
+    throwError?: boolean;
+}>;
+
+export type FetchProxyArgs = FetchArgs &
+    Readonly<{
+        cacheKey: string;
+    }>;
+
+export type MutateArgs = Readonly<{
+    method: HttpMethod;
+    url: string;
+    body: string;
+    headers?: HeadersInit;
+    signal?: AbortSignal;
+    throwError?: boolean;
+}>;
