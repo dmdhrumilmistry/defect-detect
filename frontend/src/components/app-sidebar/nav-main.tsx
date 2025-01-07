ea4ecca8236e07cdf604c779cb3bd9ea2677bde0
dhyeythumar <dhyeythumar@gmail.com>
2025-01-07 18:42:08 +0530

    fea: added app sidebar UI

---
+++ frontend/src/components/app-sidebar/nav-main.tsx
@@ -0,0 +1,34 @@
+import { type LucideIcon } from 'lucide-react';
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+type NavMainProps = {
+    items: {
+        title: string;
+        url: string;
+        icon?: LucideIcon;
+    }[];
+};
+
+export default function NavMain({ items }: NavMainProps) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarMenu>
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton tooltip={item.title}>
+                            {item.icon && <item.icon />}
+                            <span>{item.title}</span>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
+}
