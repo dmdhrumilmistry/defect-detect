b5aa0081b3ae0acacbbd1a05cddc427d1603fc18
dhyeythumar <dhyeythumar@gmail.com>
2025-01-18 16:29:35 +0530

    added actions logic

--- frontend/src/components/projects/utils.ts
+++ frontend/src/components/projects/utils.ts
@@ -1,9 +1,10 @@
-import type { LoaderFunction, LoaderFunctionArgs } from 'react-router-dom';
-import type { ProjectsLoader, RouteHandle, TProject } from '@/types';
+import type { LoaderFunction, LoaderFunctionArgs, ActionFunction, ActionFunctionArgs } from 'react-router-dom';
+import type { ProjectsDataLoader, RouteHandle, StringKVs, TProject } from '@/types';
 import { API_BASE_URL, CACHE_KEYS } from '@/services/const';
 import RestServiceProxy from '@/services/rest-proxy';
+import { sleep } from '@/lib/utils';
 
-const projectsLoader: LoaderFunction = async (args: LoaderFunctionArgs): Promise<ProjectsLoader> => {
+const projectsDataLoader: LoaderFunction = async (args: LoaderFunctionArgs): Promise<ProjectsDataLoader> => {
     console.info('[LOADER] Project(s) ::', args);
 
     const projects = await RestServiceProxy.fetch<unknown>({
@@ -17,8 +18,23 @@ const projectsLoader: LoaderFunction = async (args: LoaderFunctionArgs): Promise
     return { projects: (projects?.products ?? []) as TProject[] };
 };
 
+const projectsAction: ActionFunction = async (args: ActionFunctionArgs): Promise<StringKVs> => {
+    console.log('[ACTION] Project(s) :: ', args);
+
+    if (args.request.method === 'POST') {
+        // TODO :: handle project creation
+    } else {
+        throw new Error('Unsupported action method!');
+    }
+
+    await sleep(5);
+    // invalidate cache
+    RestServiceProxy.invalidateCache(CACHE_KEYS.projects);
+    return { success: 'true' };
+};
+
 const projectsHandle: RouteHandle = {
     breadcrumb: () => ({ href: '/projects', label: 'Projects' }),
 };
 
-export { projectsLoader, projectsHandle };
+export { projectsDataLoader, projectsAction, projectsHandle };
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function sleep(sec: number) {
+    return new Promise((resolve) => setTimeout(() => resolve('done'), sec * 1000));
+}
