c563d89445df73c6bfd79bdd2b5cb61d3bdacab2
dhyeythumar <dhyeythumar@gmail.com>
2025-01-24 20:47:23 +0530

    fea: updated create project form

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -8,3 +8,24 @@ export function cn(...inputs: ClassValue[]) {
 export function sleep(sec: number) {
     return new Promise((resolve) => setTimeout(() => resolve('done'), sec * 1000));
 }
+
+export async function convertFileToString(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        try {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                resolve(event.target?.result as string);
+            };
+            reader.readAsText(file);
+        } catch (err) {
+            console.error(err);
+            reject(new Error('Error while parsing File'));
+        }
+    });
+}
+
+export function convertStringToFile(content: string, fileName: string): File {
+    return new File([content], fileName, {
+        type: 'application/json',
+    });
+}
