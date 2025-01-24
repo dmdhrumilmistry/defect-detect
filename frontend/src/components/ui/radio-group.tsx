905530085f3350446fb5bbab215e2021134105dc
dhyeythumar <dhyeythumar@gmail.com>
2025-01-24 20:47:51 +0530

    shadcn/ui: minor updates

--- frontend/src/components/ui/radio-group.tsx
+++ frontend/src/components/ui/radio-group.tsx
@@ -20,13 +20,13 @@ const RadioGroupItem = React.forwardRef<
         <RadioGroupPrimitive.Item
             ref={ref}
             className={cn(
-                'aspect-square h-4 w-4 rounded-full border border-slate-200 border-slate-900 text-slate-900 shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-50 dark:text-slate-50 dark:focus-visible:ring-slate-300',
+                'aspect-square h-4 w-4 rounded-full border border-slate-900 text-slate-900 shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-50 dark:text-slate-50 dark:focus-visible:ring-slate-300',
                 className
             )}
             {...props}
         >
             <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-                <Circle className="h-3.5 w-3.5 fill-primary" />
+                <Circle className="h-3.5 w-3.5 fill-slate-900 dark:fill-slate-50" />
             </RadioGroupPrimitive.Indicator>
         </RadioGroupPrimitive.Item>
     );
